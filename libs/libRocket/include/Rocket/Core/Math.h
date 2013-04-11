60e98f5c28bd0554ae62f9312373b3362708cea5
pelayo <mf.pelayo@gmail.com>
2013-04-11 14:05:52 +0200

    include: comment PI / TWO_PI

--- libs/libRocket/include/Rocket/Core/Math.h
+++ libs/libRocket/include/Rocket/Core/Math.h
@@ -37,11 +37,11 @@ namespace Math {
 // The constant PI / 2.
 extern ROCKETCORE_API const float PI_BY_TWO;
 // The constant PI.
-extern ROCKETCORE_API const float PI;
+//extern ROCKETCORE_API const float PI;
 // The constant 2 * PI / 3.
 extern ROCKETCORE_API const float TWO_PI_BY_THREE;
 // The constant 2 * PI.
-extern ROCKETCORE_API const float TWO_PI;
+//extern ROCKETCORE_API const float TWO_PI;
 
 
 template < typename Type >
